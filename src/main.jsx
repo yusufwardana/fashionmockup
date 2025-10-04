962320acbdeb7c8946ba7ed9bde8b2a4401345e3
0xKeynara <75882474+yusufwardana@users.noreply.github.com>
2025-10-05 00:03:05 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-// Jika Anda memiliki file CSS utama yang mengimpor Tailwind, Anda dapat mengaktifkan ini.
-// import './index.css'; 
+// Baris ini sudah diaktifkan untuk memuat Tailwind CSS:
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
